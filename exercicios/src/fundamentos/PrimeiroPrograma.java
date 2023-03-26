503d4e799a7daac494cea6d0a24b5f72d61541b2
Fabio Oliveira <ffabioliveira81@gmail.com>
2023-03-26 10:19:34 -0300

    Primeiro programa parte #2

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,7 +2,7 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Primeiro programa!!!");
+		System.out.println("Primeiro programa Parte #1!");
+		System.out.println("Primeiro programa Parte #2!");
 	}
-
 }
