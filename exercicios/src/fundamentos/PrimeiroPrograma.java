42329e0765c1593790a9ba678e643167b2bc5ff7
Fabio Oliveira <ffabioliveira81@gmail.com>
2023-03-26 19:37:29 -0300

    Estudo de Comentários e Variaveis e Constantes

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,32 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Fábio Oliveira
+ * @since JDK1.0
+ */
+
 public class PrimeiroPrograma {
+	
+	/**
+	 * ...
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		
+
+		// Sentença de código termina com ";" (ponto e vígugula)
+		// Mais um comentário..
+		// Fim
 		System.out.println("Primeiro programa Parte #1!");
+		
+		/*
+		 * 
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3...
+		 *  
+		 */
 		System.out.println("Primeiro programa Parte #2!");
 	}
 }
